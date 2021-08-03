f257cfed8a6ddd7ca169ebce33f2da7316993398
Karen Li <karenjy.li@gmail.com>
2021-08-03 09:00:18 -0500

    Attempt to add admin login route

--- th-fe/src/App.js
+++ th-fe/src/App.js
@@ -1,27 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
-import Login from "./component/form/login"
+import AdminLoginForm from './component/form/adminLogin';
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <div className='content'>
+          <Switch>
+            <Route path='/adminlogin' component={AdminLoginForm}></Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
