a03544a4375d5e778129ee09a93eb28fe2cd698f
Karen Li <karenjy.li@gmail.com>
2021-08-10 06:32:38 -0500

    Set creds to hooks and add validation on FE

--- th-fe/src/component/auth/companyLogin.js
+++ th-fe/src/component/auth/companyLogin.js
@@ -1,19 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-import LoginForm from "./reusable/login"
+import LoginForm from './reusable/login';
 
-const handleSubmit = () => {
-    console.log('Company Login Form Submit Clicked')
-}
+const CompanyLogin = (props) => {
+  const [userName, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [hideWarning, setHideWarning] = useState(true);
 
-const CompanyLogin = props => {
-    return (
-        <div className="admin-login-form">
-            <h1>Company Login</h1>
-            <LoginForm onClick={handleSubmit}/>
-            
-        </div>
-    )
-}
+  const handleUserNameChange = (event) => {
+    setUserName(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleSubmit = () => {
+    console.log('Company Login Form Submit Clicked');
+    if (!userName || !password) {
+      setHideWarning(false);
+    } else if (password.length < 6) {  // May adjust this based on password requirement
+       setHideWarning(false);
+    } else {
+      setHideWarning(true);
+    }
+  };
+
+  return (
+    <div className='admin-login-form'>
+      <h1>Company Login</h1>
+      <LoginForm
+        onClick={handleSubmit}
+        userName={userName}
+        password={password}
+        onUserNameChange={handleUserNameChange}
+        onPasswordChange={handlePasswordChange}
+        warning={hideWarning}
+      />
+    </div>
+  );
+};
 
 export default CompanyLogin;
--- th-fe/src/component/auth/reusable/login.js
+++ th-fe/src/component/auth/reusable/login.js
@@ -7,6 +7,7 @@ import { Button } from '@material-ui/core';
 const LoginForm = (props) => {
   return (
     <div className='login-form'>
+      <h4 hidden={props.warning}>Username and Password are required.</h4>
       <form>
         <div>
           <label>Username</label>
@@ -14,7 +15,7 @@ const LoginForm = (props) => {
             type='text'
             title='userName'
             placeholder='Username'
-            onChange={props.onChange}></input>
+            onChange={props.onUserNameChange}></input>
         </div>
         <div>
           <label>Password</label>
@@ -22,13 +23,14 @@ const LoginForm = (props) => {
             type='password'
             title='userPassword'
             placeholder='Password'
-            onChange={props.onChange}></input>
+            onChange={props.onPasswordChange}
+            value={props.password}></input>
         </div>
         <Button
           variant='contained'
           color='primary'
           size='large'
-          onClick={() => props.onClick()}>
+          onClick={() => props.onClick(props.userName, props.password)}>
           Login
         </Button>
       </form>
