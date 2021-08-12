ce00218fc592744e81bee310c9027df0d247d06f
Karen Li <karenjy.li@gmail.com>
2021-08-12 06:51:43 -0500

    Add all user type dashboard routes

--- th-fe/src/App.js
+++ th-fe/src/App.js
@@ -8,7 +8,9 @@ import AdminLogin from './component/auth/adminLogin';
 import ApplicantLogin from './component/auth/applicantLogin';
 import CompanyLogin from './component/auth/companyLogin';
 
+import ApplicantDashboard from './component/dashboard/applicantDashboard';
 import CompanyDashboard from './component/dashboard/companyDashboard';
+import AdminDashboard from './component/dashboard/adminDashboard';
 
 function App() {
   return (
@@ -20,10 +22,12 @@ function App() {
             <Route path='/adminlogin' component={AdminLogin}></Route>
             <Route path='/applicantlogin' component={ApplicantLogin}></Route>
             <Route path='/companylogin' component={CompanyLogin}></Route>
+            <Route exact path='dashboard1' component={ApplicantDashboard} />
             <Route
               exact
               path='/dashboard2'
               component={CompanyDashboard}></Route>
+            <Route exact path='/dashboard3' component={AdminDashboard} />
           </Switch>
         </div>
       </div>
