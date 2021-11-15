e6d4dff1e647f51182987d5d7e31a3277ee0c062
Karen Li <karenjy.li@gmail.com>
2021-11-14 18:19:46 -0600

    Add profile complete page

---
+++ th-fe/src/component/applicant/onboarding/profileComplete.js
@@ -0,0 +1,24 @@
+import React, {useState} from "react"
+
+import { Button, FormGroup, TextField } from "@material-ui/core";
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+
+
+const profileComplete = (props) => {
+    return (
+        <div>
+            <h1>Way to go!</h1>
+            <h2>You've completed your profile.</h2>
+            <Button
+          color='primary'
+          variant='outlined'
+          endIcon={<ArrowForwardIcon />}
+          onClick={()=>props.viewProfileClick()}
+        >
+         View Profile
+        </Button>
+        </div>
+    )
+}
+
+export default profileComplete;
