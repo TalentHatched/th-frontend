8797bb14661ca6e9c9deda6b17a462165fbfeba2
Karen Li <karenjy.li@gmail.com>
2021-12-11 22:10:51 -0600

    Add support for parent contact form on applicant onboarding

--- th-fe/src/component/applicant/onboarding/parentContactForm.js
+++ th-fe/src/component/applicant/onboarding/parentContactForm.js
@@ -1,11 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { Button } from "@material-ui/core";
+import axios from "axios";
 
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
 import KeyboardBackspaceIcon from "@material-ui/icons/KeyboardBackspace";
 
 const ParentContactScreen = (props) => {
+  const [guardianInfo, setGuardianInfo] = useState({
+    guardianName: "",
+    guardianContact: "",
+    applicantId: "",
+  });
+  const [warning, setWarning] = useState("");
+
+  const { guardianName, guardianContact, applicantId } = guardianInfo;
+
+  const handleChange = (event) => {
+    console.log("What is event", event);
+    setGuardianInfo({
+      ...guardianInfo,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const onSubmitGuardianContact = () => {
+    let guardianContactInfo = {
+      applicantId: localStorage.getItem("userId"),
+      guardianName: guardianName,
+      guardianEmail: guardianContact,
+      status: "CONSENT_FORM_NOT_SENT",
+      consentReceived: false,
+    };
+
+    if (validateContact()) {
+      console.log("What is guardianContactInfo", guardianContactInfo);
+      // POST call here
+
+      axios
+        .post("api/guardian/", guardianContactInfo)
+        .then((res) => {
+          props.handleParentContactClick();
+        })
+        .catch((error) => {
+          setWarning("Error saving guardian contact. Please try again later.");
+        });
+    }
+  };
+
+  const validateContact = () => {
+    console.log("What is guardianName", guardianName);
+    if (guardianName === "" || guardianContact === "") {
+      setWarning("Parent/Guardian name and email required");
+      return false;
+    } else if (!guardianContact.includes("@")) {
+      setWarning("Valid email required");
+      return false;
+    } else {
+      return true;
+    }
+  };
+
   return (
     <div>
       <div>
@@ -19,14 +74,23 @@ const ParentContactScreen = (props) => {
         Hatched.
       </h2>
       <label>Name of parent or guardian</label>
-      <input type='text'></input>
+      <input
+        type='text'
+        name='guardianName'
+        value={guardianName}
+        onChange={handleChange}></input>
       <label>Parent or guardian email</label>
-      <input type='text'></input>
+      <input
+        type='text'
+        name='guardianContact'
+        value={guardianContact}
+        onChange={handleChange}></input>
       <div>
+        {warning ? <h4>{warning}</h4> : ""}
         <Button
           variant='contained'
           color='primary'
-          onClick={() => props.handleParentContactClick()}>
+          onClick={() => onSubmitGuardianContact()}>
           Enter name and email
         </Button>
       </div>
